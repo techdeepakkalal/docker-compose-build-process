99c1c54eac764ce5130139e94ea6d1885688dd78
deepak <kdeepak.cd@gmail.com>
2026-02-16 16:58:44 +0530

    uodated

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 //const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
-const API_BASE_URL = "http://107.23.118.185:84"
+const API_BASE_URL = "http://107.23.118.154:84"
 
 // const API_BASE_URL = "http://98.92.112.243:84"
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
